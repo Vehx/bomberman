3233b40169d922f2f93b9d5dcf7244e02a5dc1a8
marcusiscode <marcusaugustssonn@gmail.com>
2020-05-10 16:48:28 +0200

    box style33

--- resources/box.js
+++ resources/box.js
@@ -1,15 +1,30 @@
 class Box {
-    
-    width = 35
-    height= 35
+
+    width = 40
+    height = 40
     curvs = 2;
 
-  
 
-    Box(xPosition,yPosition){
-       rect(xPosition,yPosition,this.width, this.height,this.curvs) 
-        let c = color(0, 126, 255, 102);
-        fill(c);
+
+    BoxStyle(xPosition, yPosition) {
+
+        const background = color(132, 66, 4);
+        fill(background);
+        strokeWeight(1);
+        let x = xPosition;
+        let y = yPosition;
+        let y2 = y + 4;
+        let x2 = x + 4;
+        const box1size = 40;
+        const box2size = box1size - 8
+        const lines = 6;
+        rect(x, y, box1size, box1size, this.curvs)
+        rect(x2, y2, box2size, box2size)
+        for (let index = 0; index < box2size; index += box2size / lines) {
+
+            line(x2, y2 + index, box2size + x2, y2 + index);
+
+        }
     }
 
 }
