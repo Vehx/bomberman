bda2cc95fc03370bf5953e9781f760e404df8eb9
vehx <delta222@gmail.com>
2020-05-16 17:28:03 +0200

    added testing objects and logic

--- sketch.js
+++ sketch.js
@@ -4,16 +4,30 @@ function preload() {
 
 function setup() {
     createCanvas(640, 480);
-    player1 = new Player(60, 60, 35, 155);
+    player1 = new Player(60, 60, 36, 155);
     // player2 = new Player(220, 220, 35, 75);
     gameMap = new Map(height, width);
+    box1 = new Box(40, 120, 80);
+    wall1 = new Wall(40, 80, 80);
     const generatedMap = gameMap.generate();
+    console.log(box1.x, box1.y);
 }
 
 function draw() {
     gameMap.draw();
+    box1.BoxStyle();
+    wall1.draw();
+    let p1x = player1.posistionX();
+    let p1y = player1.posistionY();
     player1.playerWalk();
+    if (player1.intersects(box1) || player1.intersects(wall1)) {
+        // if (player1.intersects(box1)) {
+        player1.x = p1x;
+        player1.y = p1y;
+        console.log("intersect");
+    }
     player1.playerShow();
+
     // player2.playerWalk();
     // player2.playerShow();
 }
