cacd5200cd9ef00829cd02aef0c177e4b4c2a8e1
vehx <delta222@gmail.com>
2020-05-16 15:08:53 +0200

    added test player

--- sketch.js
+++ sketch.js
@@ -4,10 +4,16 @@ function preload() {
 
 function setup() {
     createCanvas(640, 480);
+    player1 = new Player(60, 60, 35, 155);
+    // player2 = new Player(220, 220, 35, 75);
     gameMap = new Map(height, width);
     const generatedMap = gameMap.generate();
 }
 
 function draw() {
     gameMap.draw();
+    player1.playerWalk();
+    player1.playerShow();
+    // player2.playerWalk();
+    // player2.playerShow();
 }
